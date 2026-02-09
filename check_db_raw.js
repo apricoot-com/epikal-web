96edeff437c3db91b9b9703edaef6c1658cc1001
Dickinson Arismendy <thordickinson@gmail.com>
2026-02-09 16:37:24 -0500

    fix: check db_raw

--- check_db_raw.js
+++ check_db_raw.js
@@ -19,6 +19,15 @@ async function main() {
         `);
         console.table(enums.rows);
 
+        console.log("--- COMPANIES COLUMNS ---");
+        const resComp = await client.query(`
+            SELECT column_name, data_type 
+            FROM information_schema.columns 
+            WHERE table_name = 'companies'
+            ORDER BY column_name;
+        `);
+        console.table(resComp.rows);
+
         console.log("--- SERVICES COLUMNS ---");
         const res = await client.query(`
             SELECT column_name, data_type 
@@ -27,19 +36,6 @@ async function main() {
             ORDER BY column_name;
         `);
         console.table(res.rows);
-
-        console.log("--- TEST SELECT * FROM services ---");
-        const res3 = await client.query('SELECT * FROM services LIMIT 1');
-        console.table(res3.rows);
-
-        console.log("--- SERVICE_WEB_PAGES COLUMNS ---");
-        const res2 = await client.query(`
-            SELECT column_name, data_type 
-            FROM information_schema.columns 
-            WHERE table_name = 'service_web_pages'
-            ORDER BY column_name;
-        `);
-        console.table(res2.rows);
 
     } catch (err) {
         console.error(err);
